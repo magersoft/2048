928f612c70added15d848a96f6047247a463428d
magersoft <magervlad@yandex.ru>
2023-04-20 11:17:07 +0800

    master to main

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:magersoft/2048.git master:gh-pages
+git push -f git@github.com:magersoft/2048.git main:gh-pages
 cd -
