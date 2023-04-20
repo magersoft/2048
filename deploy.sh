7571b24f871c02b5c1e9d23c2c124992f789a0a1
magersoft <magervlad@yandex.ru>
2023-04-20 11:09:05 +0800

    prepared for deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:magersoft/2048.git master:gh-pages
+cd -
